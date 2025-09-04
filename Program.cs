c93bdb8bbee2ca797564d8106c1d037bfa17ce35
Daniel Diaz <danielodiazo@gmail.com>
2025-09-04 17:25:05 +0200

    Añadi varios renglones

--- Program.cs
+++ Program.cs
@@ -11,6 +11,11 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hola mundo");
+            Console.WriteLine("Hola mundo");
+            Console.WriteLine("Hola mundo");
+            Console.WriteLine("Hola mundo");
+            Console.WriteLine("Hola mundo");
+            Console.WriteLine("Hola mundo");
         }
     }
 }
